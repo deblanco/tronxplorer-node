ecfba7e4a90cd362ff1c8cae482194874f2acb60
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-08 23:42:02 +0200

    Added methods to retrieve witnesses

--- controllers/NetworkController.js
+++ controllers/NetworkController.js
@@ -5,10 +5,29 @@ const TronClient = new GrpcClient({
   hostname: CONFIG.tron_node,
 });
 
-const getNodes = () => NetworkInstance.nodes;
-const getWitnesses = () => NetworkInstance.witnesses;
+const getNodes = async (req, res) => {
+  const [err, fNodes] = await to(TronClient.getNodes());
+  return ReS(res, { nodes: fNodes });
+};
+
+const getWitnesses = async (req, res) => {
+  const [err, fWitnesses] = await to(TronClient.getWitnesses());
+  return ReS(res, { witnesses: fWitnesses });
+};
+
+const getWitness = async (req, res) => {
+  const { address } = req.params;
+  // validation
+  if (!address || address.length === '') {
+    return ReE(res, 'Must send a address as argument.');
+  }
+  const [err, fWitnessess] = await to(TronClient.getWitnesses());
+  const witnessMatch = fWitnessess.find(witness => witness.address === address);
+  return ReS(res, { witness: witnessMatch });
+};
 
 module.exports = {
   getNodes,
   getWitnesses,
+  getWitness,
 };
--- routes/v1.js
+++ routes/v1.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const BlockController = require('./../controllers/BlockController');
 const AccountController = require('./../controllers/AccountController');
 const TokenController = require('./../controllers/TokenController');
+const NetworkController = require('./../controllers/NetworkController');
 
 router.get('/transactions/:address', BlockController.getTransactions);
 router.get('/transactions/last/:limit', BlockController.getLastestTransactions);
@@ -15,8 +16,8 @@ router.get('/blocks/last/:limit', BlockController.getLastestBlocks);
 router.get('/account/:address', AccountController.getAccount);
 router.get('/accounts', AccountController.getAccounts);
 
-router.get('/witnesses', AccountController.getAccounts);
-router.get('/witnesses/:address', AccountController.getAccounts);
+router.get('/witnesses', NetworkController.getWitnesses);
+router.get('/witnesses/:address', NetworkController.getWitness);
 
 router.get('/tokens', TokenController.getTokens);
 router.get('/tokens/:name', TokenController.getToken);
