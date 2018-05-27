e22a869bfb940e006e36b5df2c6dafabe4228588
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-27 18:29:34 +0200

    BlockContrller changed to get info from SolidityNode

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -1,9 +1,9 @@
 require('./../global_functions');
-const GrpcClient = require('@tronprotocol/wallet-api/src/client/grpc');
+const SolidityClient = require('@tronprotocol/wallet-api/src/client/solidity_grpc');
 
-const TronClient = new GrpcClient({
-  hostname: CONFIG.tron_node,
-  port: CONFIG.tron_node_port,
+const TronClient = new SolidityClient({
+  hostname: CONFIG.solidity_node,
+  port: CONFIG.solidity_node_port,
 });
 
 const getTokens = async (req, res) => {
