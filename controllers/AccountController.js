9789917677896b91e8293ea5200b4f11034ead81
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-27 18:24:28 +0200

    fixed getAccounts

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,9 +1,10 @@
+const SolidityClient = require('@tronprotocol/wallet-api/src/client/solidity_grpc');
+const { Account } = require('./../models');
 require("./../global_functions");
-const GrpcClient = require('@tronprotocol/wallet-api/src/client/grpc');
 
-const TronClient = new GrpcClient({
-  hostname: CONFIG.tron_node,
-  port: CONFIG.tron_node_port,
+const TronClient = new SolidityClient({
+  hostname: CONFIG.solidity_node,
+  port: CONFIG.solidity_node_port,
 });
 
 const getAccount = async (req, res) => {
@@ -17,7 +18,7 @@ const getAccount = async (req, res) => {
 };
 
 const getAccounts = async (req, res) => {
-  const [err, fAccounts] = await to(TronClient.getAccounts());
+  const [err, fAccounts] = await to(Account.find({}));
   ReS(res, { accounts: fAccounts });
 };
 
