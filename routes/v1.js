6cbf9e3ffc643eb0310efa6c1325a5b95c6ea4bc
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-29 22:12:36 +0200

    Added cache to routing

--- routes/v1.js
+++ routes/v1.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const apicache = require('apicache');
 
 const router = express.Router();
+const cache = apicache.middleware;
 
 const BlockController = require('./../controllers/BlockController');
 const AccountController = require('./../controllers/AccountController');
@@ -9,22 +11,22 @@ const NetworkController = require('./../controllers/NetworkController');
 const SearchController = require('./../controllers/SearchController');
 const TransactionController = require('./../controllers/TransactionController');
 
-router.get('/transaction/:transactionHash', TransactionController.getTransaction);
-router.get('/transactions/:address', TransactionController.getTransactions);
-router.get('/transactions/last/:limit', TransactionController.getLastestTransactions);
-router.get('/transactions/list/:limit', TransactionController.getTransactionList);
+router.get('/transaction/:transactionHash', cache('1 day'), TransactionController.getTransaction);
+router.get('/transactions/:address', cache('1 minute'), TransactionController.getTransactions);
+router.get('/transactions/last/:limit', cache('15 seconds'), TransactionController.getLastestTransactions);
+router.get('/transactions/list/:limit', cache('15seconds'), TransactionController.getTransactionList);
 
-router.get('/blocks/:block', BlockController.getBlock);
-router.get('/blocks/last/:limit', BlockController.getLastestBlocks);
+router.get('/blocks/:block', cache('1 day'), BlockController.getBlock);
+router.get('/blocks/last/:limit', cache('15 seconds'), BlockController.getLastestBlocks);
 
-router.get('/account/:address', AccountController.getAccount);
-router.get('/accounts', AccountController.getAccounts);
+router.get('/account/:address', cache('1 minute'), AccountController.getAccount);
+router.get('/accounts', cache('15 seconds'), AccountController.getAccounts);
 
-router.get('/witnesses', NetworkController.getWitnesses);
-router.get('/witnesses/:address', NetworkController.getWitness);
+router.get('/witnesses', cache('15 seconds'), NetworkController.getWitnesses);
+router.get('/witnesses/:address', cache('2 hours'), NetworkController.getWitness);
 
-router.get('/tokens', TokenController.getTokens);
-router.get('/tokens/:name', TokenController.getToken);
+router.get('/tokens', cache('15 seconds'), TokenController.getTokens);
+router.get('/tokens/:name', cache('2 hours'), TokenController.getToken);
 
 router.get('/search', SearchController.queryFor);
 
