f136c5d4069a8725886093f19a930ac01d8ce56a
Daniel Blanco Parla <dblancoparla@gmail.com>
2018-05-30 08:54:58 +0200

    added cache to Account

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,5 +1,6 @@
 const SolidityClient = require('@tronprotocol/wallet-api/src/client/solidity_grpc');
 const { Account } = require('./../models');
+const cache = require('memory-cache');
 require("./../global_functions");
 
 const TronClient = new SolidityClient({
@@ -7,18 +8,28 @@ const TronClient = new SolidityClient({
   port: CONFIG.solidity_node_port,
 });
 
+const fetchCache = async (strCached, asyncFn, time = 60000) => {
+  const isCached = cache.get(strCached);
+  if (isCached) {
+    return isCached;
+  }
+  const fetchFn = await asyncFn;
+  cache.put(strCached, fetchFn, time);
+  return fetchFn;
+};
+
 const getAccount = async (req, res) => {
   const { address } = req.params;
   // validation
   if (!address || address.length < 35) {
     return ReE(res, "The account must have 35 characters.");
   }
-  const [err, fAccount] = await to(TronClient.getAccount(address));
+  const [err, fAccount] = await to(fetchCache('address', TronClient.getAccount(address)));
   ReS(res, { account: fAccount });
 };
 
 const getAccounts = async (req, res) => {
-  const [err, fAccounts] = await to(Account.find({}));
+  const [err, fAccounts] = await to(fetchCache('allAccounts', Account.find({})));
   ReS(res, { accounts: fAccounts });
 };
 
